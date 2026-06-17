8f62f629b5c47794565b2e1eee550e87c0bdebf3
Moon-codes-26 <aparnasr2007@gmail.com>
2026-06-17 22:14:58 +0530

    updated deployment

--- src/pages/VideoPage.jsx
+++ src/pages/VideoPage.jsx
@@ -3,16 +3,22 @@ import videos from "../data/videos";
 
 function VideoPage() {
   const { id } = useParams();
-  const video = videos.find(v => v.id == id);
+
+  const video = videos.find(v => String(v.id) === String(id));
+
+  if (!video) {
+    return <h2 style={{ color: "white" }}>Video not found</h2>;
+  }
 
   return (
-    <div>
+    <div style={{ color: "white", padding: "20px" }}>
       <h2>{video.title}</h2>
 
       <iframe
         width="700"
         height="400"
         src={`https://www.youtube.com/embed/${video.videoId}`}
+        title={video.title}
         allowFullScreen
       />
     </div>
